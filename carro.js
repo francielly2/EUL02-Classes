08c8f69257449f64cbb53081f3a7c9b240496402
fran <francielly2403@gmail.com>
2025-10-28 12:34:06 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,12 @@
+var valor1, valor2, valor3;
+
+if (submit) true; {
+    document.getElementById("submit").innerHTML = function() {
+        valor1 = document.getElementById("carro").value;
+        valor2 = document.getElementById("modelo").value;
+        valor3 = document.getElementById("versao").value;
+    }
+}
+
+
+        
